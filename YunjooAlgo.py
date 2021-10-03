b3c46b146fc999b6bfdd1097b02252ce8f853fff
YoungchoKim <dudch91@naver.com>
2021-10-03 22:55:30 +0900

    bug fix. min -> min1

--- YunjooAlgo.py
+++ YunjooAlgo.py
@@ -32,12 +32,12 @@ class YunjooAlgo(TradeAlgorithm):
         self.pInfo.set_tickers(self.ticker_list)
         logging.info('ticker list:{}, len:{}'.format(self.ticker_list, len(self.ticker_list)))
 
-    def check_buy_coin(self, df_day, df_min, ticker, ticket_list):
+    def check_buy_coin(self, df_day, df_min1, ticker, ticket_list):
         success = True
 
         before25 = df_day.iloc[0, 3]
         before5 = df_day.iloc[19, 3]
-        current = df_min.iloc[0, 3]
+        current = df_min1.iloc[0, 3]
         if before25 * 0.8 < before5:
             return
         
@@ -69,14 +69,14 @@ class YunjooAlgo(TradeAlgorithm):
         ticket_list = []
         for ticker in self.ticker_list:
             df_day = self.ohlcv['day'][ticker]
-            df_min = self.ohlcv['min'][ticker]
-            if 'error' in df_day or 'error' in df_min:
+            df_min1 = self.ohlcv['min1'][ticker]
+            if 'error' in df_day or 'error' in df_min1:
                 logging.info(df_day['error'])
                 continue
             df_day = df_day.tail(25)
-            df_min = df_day.tail(1)
+            df_min1 = df_day.tail(1)
             if ticker not in [coin.get_ticker() for coin in have_coin_list]:
-                self.check_buy_coin(df_day, df_min, ticker, ticket_list)
+                self.check_buy_coin(df_day, df_min1, ticker, ticket_list)
             else:
                 self.check_sell_coin(ticker, ticket_list, have_coin_list)
         if len(ticket_list) != 0:    
